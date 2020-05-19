f094e786c08e8bf3678d4b5f126dd11dc8b4c957
zeroclock <zeroclock@icloud.com>
2020-05-19 09:03:56 +0900

    pass file to webassembly

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,35 @@
 mod utils;
 
 use wasm_bindgen::prelude::*;
+use js_sys::ArrayBuffer;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+pub enum FileType {
+    PNG,
+    JPG
+}
+
+#[wasm_bindgen]
+pub struct InputFile {
+    fileType: FileType,
+    buffer: ArrayBuffer
+}
+
+#[wasm_bindgen]
+impl InputFile {
+    pub fn new(fileType: FileType, buffer: ArrayBuffer) -> InputFile {
+        InputFile {
+            fileType,
+            buffer
+        }
+    }
+}
 
 #[wasm_bindgen]
 extern {
@@ -16,4 +39,9 @@ extern {
 #[wasm_bindgen]
 pub fn greet(s: &str) {
     alert(s);
+}
+
+#[wasm_bindgen]
+pub fn ocr(input: InputFile) {
+    alert(input.buffer.byte_length().to_string().as_str());
 }
--- www/src/components/app/dropzone/index.tsx
+++ www/src/components/app/dropzone/index.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
 import { useDropzone } from 'react-dropzone'
 import styled from 'styled-components'
-import * as wasm from 'everyones-ocr'
+import { FileType, InputFile, ocr } from 'everyones-ocr'
 
 const Container = styled.div`
   border: 0.375rem dashed #2699FB76;
@@ -24,28 +24,17 @@ const Description = styled.p`
   padding: 0 50px;
 `
 
-enum FileType {
-  PNG,
-  JPG,
-}
-
-interface InputData {
-  fileType: FileType,
-  contentData: ArrayBuffer
-}
-
 const DropZone = () => {
   const { acceptedFiles, getRootProps, getInputProps } = useDropzone()
   const reader = new FileReader();
 
-  const getFileData = (file: File): InputData | null => {
+  const inputHandler = (file: File): void => {
     reader.onload = (_) => {
       const fileType = getFileType(file.type)
       const result = reader.result
-      if (result instanceof ArrayBuffer) {
-        return { fileType, contentData: result }
-      } else {
-        return null
+      if (result instanceof ArrayBuffer && fileType !== null) {
+        const inputFile = InputFile.new(fileType, result)
+        ocr(inputFile)
       }
     }
     reader.readAsArrayBuffer(file)
@@ -63,15 +52,8 @@ const DropZone = () => {
   }
 
   useEffect(() => {
-    reader.onload = (e) => {
-      const result = reader.result
-      if (typeof result === 'string') {
-        wasm.greet(result)
-      }
-    }
     acceptedFiles.map(file => {
-      console.log(file.type)
-      // reader.readAsText(file)
+      inputHandler(file)
     })
   }, [acceptedFiles])
 
